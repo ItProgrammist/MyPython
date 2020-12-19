965fcf9b4b2a68eedad3b4c37eee5141d7785ed1
ItProgrammist <50521864+ItProgrammist@users.noreply.github.com>
2020-12-19 20:01:53 +0700

    Update base1.py

--- base1.py
+++ base1.py
@@ -19,11 +19,46 @@
 #     age = (input("Введите возраст: "))
 
 
-n = int(input("Введите год: "))
-if (n >= 0):
-    if(n%4 == 0 or n%400 == 0) and (n%100 != 0):
-        print("YES")
-    else:
-        print("NO")
+# n = int(input("Введите год: "))
+# if (n >= 0):
+#     if(n%4 == 0 or n%400 == 0) and (n%100 != 0):
+#         print("YES")
+#     else:
+#         print("NO")
+# else:
+#     print("Что за год такой?")
+
+# A = int(input("Enter A: "))
+# B = int(input("Entr B: "))
+
+# if A <= B:
+#     for i in range(A,B+1,2):
+#         print(A," ")
+#         A-=-2
+# if A>=B:
+#     for i in range(A,B-2,-2):
+#         print(A," ")
+#         A-=2
+
+# n = int(input("Enter n: "))
+# count = "1"
+# if(n <= 9):
+#     for i in range(1,n+1):
+#         print(count)
+#         count = count+str(i+1)
+# else:
+#     print("n больше 9")
+
+
+n = int(input("Enter n: "))
+count = " "
+count = count*(n-1)
+count_2 = "*"
+
+if(n <= 9):
+    for i in range(1,n+1):
+        print(count,count_2)
+        count = count*(n-(i+1))
+        count_2 = count_2*(i+1)
 else:
-    print("Что за год такой?")
+    print("n больше 9")
