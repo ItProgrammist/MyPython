08d9731f992cca1fb36f0be322281804e0a062e6
ItProgrammist <50521864+ItProgrammist@users.noreply.github.com>
2020-12-19 19:13:32 +0700

    Update base1.py

--- base1.py
+++ base1.py
@@ -21,7 +21,7 @@
 
 n = int(input("Введите год: "))
 if (n >= 0):
-    if(n%4 == 0) or (n%400 == 0) and (n%100 != 0):
+    if(n%4 == 0 or n%400 == 0) and (n%100 != 0):
         print("YES")
     else:
         print("NO")
